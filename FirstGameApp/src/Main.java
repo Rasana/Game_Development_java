bc8902dcde202571c46042e547a153a96979d42f
codeezer <code_ezer@live.com>
2015-07-08 19:27:13 +0545

    la gare

--- FirstGameApp/src/Main.java
+++ FirstGameApp/src/Main.java
@@ -8,17 +8,13 @@ public static void main( String args[])
 	System.out.println("Almost ?? :D :D :D #ezer");
 	System.out.println("Lab bata push :D :D ");
 	System.out.println("GITHUB TESTING");
-<<<<<<< HEAD
 	System.out.println("Aba Start garam HALKA :) #ezer");
 	System.out.println("Sagar");
-=======
 	System.out.print("HAHAHAHAHAHAHAHAHAHEZER");
 	System.out.println("Sagar");
-
 	System.out.print("HAHAHAHAHAHAHAHAHAHEZER");
         System.out.print("HAHAHAHAHAHAHAHAHAHEZER");
         System.out.println("I am here");
->>>>>>> 65c63dc4e4d191003c328ec74df7e88cbe91d675
 }
 }
 
